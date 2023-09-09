e61646ae411ecb51fad72c0c71a9e8b9f479711c
Bielsyah <135860450+bielsyah@users.noreply.github.com>
2023-09-09 23:03:29 +0700

    Add files via upload

---
+++ Console_serialCom/Console_serialCom/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace serialCom
+{
+    class serialCommunication
+    {
+        static SerialPort port;
+
+        static void Main(string[] args)
+        {
+           port = new SerialPort("COM3", 9600);
+            port.DtrEnable = true;
+            port.DataReceived += myData;
+            port.Open();
+
+            while(true)
+            {
+                string dataToSend = Console.ReadLine();
+                port.Write(dataToSend); 
+            }
+
+            //Console.ReadKey();
+            //port.Close();
+        }
+
+        private static void myData(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = port.ReadExisting();
+            Console.Write(data);
+        }
+    }
+}
